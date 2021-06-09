71dc30bfe35caa735a6c06113b02588f9c9018b2
Ryan Scott <rscott@bestcompany.com>
2021-06-09 10:04:14 -0600

    Custom error pages

--- app/__init__.py
+++ app/__init__.py
@@ -20,4 +20,4 @@ login.login_message = 'Please log in to access this page'
 imagefiles = UploadSet('images', IMAGES)
 configure_uploads(app, imagefiles)
 
-from app import routes, models
+from app import routes, models, errors
